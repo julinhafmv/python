5f4dfefc0e7ea903c0e72a93f2dd4125c31f8ffa
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2024-03-07 12:10:28 -0300

    Add files via upload

---
+++ trabalahoPDS.py
@@ -0,0 +1,30 @@
+1//
+
+nota1 = 40
+nota2 = 50
+nota3 = 60
+    input("Digite a nota desejada:")
+    print(f"Sua nota é nota um é de {nota1}")
+
+2//
+
+if([0,2,4,6,8]) = par
+    elif ([1,3,5,7,9]) = impar
+
+input("Digite um numero de 0-10:")
+print(f"seu numero é {}")
+ 
+ /*
+
+ num1 = 10
+ num2 = 15
+
+ multiplicacao = num1 * num2 = 
+
+ soma = num1 + num2 = 
+
+ sub = num1 - num2 = 
+
+ divisao = num1 / num2 = 
+ 
+ */
