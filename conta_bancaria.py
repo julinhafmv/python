1fdb5e4a9d0337dd514b2f69b62c307fdb4044ca
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2024-04-11 08:06:13 -0300

    Add files via upload

---
+++ conta_bancaria.py
@@ -0,0 +1,8 @@
+class ContaBancaria:
+    def __init__(self,numero,titular,saldo = 0)
+        self.numero = numero 
+        self.titular = titular
+        self.saldo = saldo 
+    
+    def deposita(self,valor):
+        self.saldo += valor
