6d5422cd66f7f035b27b08f4386bc4af5c7dcd60
julinhafmv <128698307+julinhafmv@users.noreply.github.com>
2024-04-18 12:00:45 -0300

    Add files via upload

---
+++ revisao.py
@@ -0,0 +1,35 @@
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+operacao = input("Escolha a operação:")
+
+def somar(): 
+    soma = (num1 + num2)
+    print("Soma:", soma)
+
+def subtrair():
+    subtracao = (num1 - num2)
+    print("Subtração:", subtracao)
+
+def multiplicar():
+    multiplicacao = (num1 * num2)
+    print("Multiplicação:", multiplicacao)
+
+def dividir():
+    divisao = (num1 / num2)
+    print("Divisão:", divisao)
+
+if operacao == "+":
+    somar{}
+
+elif operacao == "-":
+    subtrair{}
+
+elif operacao == "*":
+    multiplicar{}
+
+elif operacao == "/":
+    dividir{}
+
+
